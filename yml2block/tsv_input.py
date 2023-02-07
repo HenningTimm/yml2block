bbf4a0a835a9f7eed69e30178bad1f179c625771
Henning Timm <henning.timm@uni-due.de>
2023-02-07 19:40:13 +0100

    Refactoring and docs

--- yml2block/tsv_input.py
+++ yml2block/tsv_input.py
@@ -7,17 +7,6 @@ import itertools
 
 from yml2block.rules import LintViolation
 
-# def _trim_to_header(line, length):
-#     """Trim leading and trailing tabs from a tsv chunk to given nr of columns.
-
-#     The three parts of a Dataverse TSV file are themselves valid tsv.
-#     However, they all start with leadingall lines but the header start
-#     with a leading empty column and the shorter sections contain several
-#     empty trailing columns.
-#     """
-#     _leading_tab, *fields = line.strip("\n").split("\t")
-#     return "\t".join(fields[:length])
-
 
 def _identify_break_points(full_file):
     """Identify where to split the metadata block into its three subsections"""
@@ -49,11 +38,14 @@ def _identify_break_points(full_file):
 
 
 def read_tsv(tsv_path):
+    """Read in a Dataverse TSV metadata block file."""
     violations = []
+    data = dict()
 
     with open(tsv_path, "r") as raw_file:
         full_file = raw_file.readlines()
 
+    # Split metadata schema into blocks
     split_blocks, break_point_violations = _identify_break_points(full_file)
     violations.extend(break_point_violations)
 
@@ -66,39 +58,44 @@ def read_tsv(tsv_path):
         joined = io.StringIO("\n".join(block))
         return csv.DictReader(joined, delimiter="\t")
 
-    md_fields, ds_fields, vocab_fields = [_parse(block) for block in split_blocks]
-    data = dict()
+    # Unpack each tsv-chunk of the metadata block into a list
+    # of dictionaries.
+    parsed_blocks = [_parse(block) for block in split_blocks]
 
-    def print_dict(d):
-        for r in d:
-            print(r)
+    for row in itertools.chain(*parsed_blocks):
+        # Each row corresponds to a line in the TSV file
+        # unpacked into a dictionary with keys depending
+        # on the part of the block identified by the top level keyword
 
-    # print_dict(md_fields)
-    # print_dict(ds_fields)
-    # print_dict(vocab_fields)
-
-    for row in itertools.chain(*(md_fields, ds_fields, vocab_fields)):
-        toplevel_key_with_hash = [
+        # Get the toplevel keyword from the first column of the TSV file
+        # e.g. #metadataBlock, #datasetField, #controlledVocabulary
+        toplevel_key_with_prefix = [
             entry for entry in row.keys() if entry is not None and entry.startswith("#")
         ][0]
-        toplevel_key = toplevel_key_with_hash.lstrip("#")
+
+        # For consistency with the yaml format
+        toplevel_key = toplevel_key_with_prefix.lstrip("#")
         row_as_dict = dict()
 
         for key, value in row.items():
             if key is None:
+                # These entries cannot be associated with a column header
                 violations.append(
                     LintViolation("ERROR", "read_tsv", "Entry in headerless column")
                 )
-            elif key is toplevel_key_with_hash:
+            elif key is toplevel_key_with_prefix:
+                # Skip toplevel header identifiers. These columns are empty in
+                # Dataverse TSV files
                 continue
             else:
+                # Copy all other entries into a new data structure for this row
                 row_as_dict[key] = value
+
+        # Initialize the entry for this toplevel keyword with an empty list
         if not (toplevel_key in data.keys()):
             data[toplevel_key] = []
-        data[toplevel_key].append(row_as_dict)
 
-    print(data)
-    # clip off keyword
+        data[toplevel_key].append(row_as_dict)
 
     # Run top_level_keywords_valid here to make sure that the generated
     # data structure behaves as expected down the line
