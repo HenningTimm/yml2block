a25b9c35dbb4f19a71e4861abe3b38e357fdfa70
Henning Timm <henning.timm@uni-due.de>
2023-03-02 10:05:19 +0100

    Documentation

--- yml2block/prefix_analysis.py
+++ yml2block/prefix_analysis.py
@@ -9,21 +9,26 @@ def split_by_common_prefixes(keywords, threshold=3, verbose=False):
     This has a worst case runtime of O(n^2) for the case where
     no prefix is shared.
 
-    Note that currently this approach greedily binds to the first prefix.
+    This approach greedily binds to the longest common prefix.
     This means that
 
       - "FooBarLong"
       - "FooBarShort"
       - "FooBarLonger"
 
-    Would report one group containing all three keywords with the common
-    prefix "FooBar". The other possible solution returning two groups
-    (one containing "FooBarLong" and "FooBarLonger" with a prefix "FooBarLong"
-    and the second group containing only "FooBarShort") can currebtly be achieved
-    using a threshold.
+    Would report two groups, one with the prefix "FooBarLong" containing
+    "FooBarLong" and "FooBarLonger", the other group with prefix "FooBarShort"
+    containing only the one item "FooBarShort".
 
-    However, to identify typos it would be helpful to bind to the longest
-    prefix instead.
+    This behavor can result in false positive errors, when two or more entries
+    share a slightly longer prefix by cnhance. For Example
+
+      - "FooBarBaz"
+      - "FooBarBest"
+      - "FooBarTest1"
+
+    Would report two groups, one with the prefix "FooBarB", even though semantically
+    one could reasonably expect that these three entries fall into the same cathegory.
     """
     identified_groups = {}
     if verbose:
@@ -43,7 +48,8 @@ def split_by_common_prefixes(keywords, threshold=3, verbose=False):
             common_prefix = os.path.commonprefix((selected_keyword, kw))
             if verbose:
                 print(
-                    f"Checking keyword {kw} against selected keyword {selected_keyword} with current prefix {current_prefix}, found common prefix {common_prefix}"
+                    f"Checking keyword {kw} against selected keyword {selected_keyword} with "
+                    f"current prefix {current_prefix}, found common prefix {common_prefix}"
                 )
 
             if current_prefix is None and len(common_prefix) >= threshold:
@@ -57,14 +63,15 @@ def split_by_common_prefixes(keywords, threshold=3, verbose=False):
                 current_prefix
             ):
                 # The detected common prefix is a superset of the expected prefix.
-
-                current_prefix = common_prefix
                 current_group.append(kw)
+
+                # Filter keywords back out of the active list, that only share
+                # a shorter common prefix. THis ensures that the longest possible prefix is chosen.
+                current_prefix = common_prefix
                 current_group = [
                     k for k in current_group if k.startswith(current_prefix)
                 ]
 
-        print()
         # Remove all keywords sharing a prefix with the selected reference keyword from the keywords list.
         keywords = [kw for kw in keywords if kw not in current_group]
         if current_prefix is None:
